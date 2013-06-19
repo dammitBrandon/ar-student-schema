766b525238d64335120cc728be8138e46314075a
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-19 17:52:58 -0500

    The completed challenge for todays DB Drill - AR Student Schema Challenge

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,21 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
+  validates :email, :format => { :with => /.*@.{1,}\..{2,}/,
+    :message => "Valid email addresses only"}
+  validates :age, :numericality => {:greater_than => 5}
+  validates :email, :uniqueness => true 
+  validates :phone, :format => { :with => /\(?\d{3}\)?.\d{3}.\d{4}/,
+    :message => "Valid phone number only"}
+  def name 
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def age
+    Time.now.year - self.birthday.year - ((Time.now.month > self.birthday.month || (Time.now.month == self.birthday.month && Time.now.day >= self.birthday.day)) ? 0 : 1)
+    #now = Time.now
+    #now - self.date
+    #now.year - self.birthday.year - ((now.month > self.birthday.month || (now.month == self.birthday.month && now.day >= self.birthday.day)) ? 0 : 1)
+  end
 # implement your Student model here
 end
--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -1,9 +1,18 @@
-require_relative '../config'
+require_relative '../config' 
+
 
 # this is where you should use an ActiveRecord migration to 
 
 class CreateStudents < ActiveRecord::Migration
   def change
+    create_table :students do |t|
+      t.string :first_name
+      t.string :last_name
+      t.date :birthday
+      t.string :gender
+      t.string :email
+      t.string :phone
     # HINT: checkout ActiveRecord::Migration.create_table
+    end
   end
 end
